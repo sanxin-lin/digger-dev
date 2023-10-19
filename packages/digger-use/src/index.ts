33fd1ad3f38b464ec87a44aa75a15e473659eb0e
linsanxin <857287645@qq.com>
2023-10-19 21:41:22 +0800

    feat: digger-use和digger-shared

---
+++ packages/digger-use/src/index.ts
@@ -0,0 +1,7 @@
+export * from './onSmartMounted';
+export * from './onSmartUnmounted';
+export * from './onWindowResize';
+export * from './useClickOutside';
+export * from './useEventListener';
+export * from './useId';
+export * from './useInitialized';
---
+++ packages/digger-use/src/useClickOutside.ts
@@ -0,0 +1,25 @@
+import { useEventListener } from './useEventListener.js';
+import { inBrowser, isFunction } from '@digger/shared';
+import { unref, type Ref } from 'vue';
+
+export type UseClickOutsideTarget = Element | Ref<Element | undefined | null> | (() => Element);
+
+export function useClickOutside(
+  target: UseClickOutsideTarget,
+  type: string,
+  listener: EventListener,
+) {
+  if (!inBrowser()) {
+    return;
+  }
+
+  const handler = (event: Event) => {
+    const element = isFunction(target) ? target() : unref(target);
+
+    if (element && !element.contains(event.target as Node)) {
+      listener(event);
+    }
+  };
+
+  useEventListener(document, type, handler);
+}
